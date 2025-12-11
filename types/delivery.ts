8a89a6e586945c253e6f01d4eced326991299f00
7u4n M1nh <tuanminh261204@gmail.com>
2025-12-11 17:21:35 +0700

    Add files via upload

--- types/delivery.ts
+++ types/delivery.ts
@@ -1,1 +1,18 @@
+export enum DeliveryStatus {
+  PENDING = 'PENDING',
+  IN_TRANSIT = 'IN_TRANSIT',
+  OUT_FOR_DELIVERY = 'OUT_FOR_DELIVERY',
+  DELIVERED = 'DELIVERED',
+  CANCELLED = 'CANCELLED',
+}
 
+export interface DeliveryData {
+  trackingId: string
+  recipient: string
+  destination: string
+  items: string
+  status: DeliveryStatus
+  owner: string
+  createdAt: number
+  deliveredAt?: number
+}
